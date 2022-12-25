e2878214d83d4dc05b11c3dd5118df8c279323e8
Nguyen Phuoc Trong <phuoctrong1001@gmail.com>
2022-12-25 23:31:32 +0700

    Add Models, ViewModels, Views, Controllers, Enums and Utils

---
+++ ViewModels/BookGenreViewModel.cs
@@ -0,0 +1,17 @@
+﻿using FPTBook.Models;
+
+using Microsoft.AspNetCore.Http;
+
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace FPTBook.ViewModels
+{
+    public class BookGenreViewModel
+    {
+        public Category Category { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+        [Display(Name = "File")]
+        public IFormFile FormFile { get; set; }
+    }
+}
