a7ddc3739c8a82716a5ea1710cb23e20d2b3a7c2
HoanNV081001 <hoannv081001@gmail.com>
2022-12-27 16:31:55 +0700

    Add Front-end to the whole system

--- Controllers/StoreOwnerController.cs
+++ Controllers/StoreOwnerController.cs
@@ -23,25 +23,24 @@ namespace FPTBook.Controllers
         {
             _context = context;
         }
-        public IActionResult Index(string genre)
+        public IActionResult Index(string word)
         {
+            if (!string.IsNullOrWhiteSpace(word))
+            {
+                var result = _context.Books
+                  .Include(t => t.Genre)
+                  .Where(t => t.Title.Contains(word) || t.Genre.Description.Contains(word))
+                  .ToList();
 
+                return View(result);
+            }
 
-          if (!string.IsNullOrWhiteSpace(genre))
-          {
-            var result = _context.Books
-              .Include(t => t.Genre)
-              .Where(t => t.Genre.Description.Equals(genre))
-              .ToList();
-
-            return View(result);
-          }
             IEnumerable<Book> books = _context.Books
             .Include(t => t.Genre)
             .ToList();
-            return View(books);
 
-          }
+            return View(books);
+        }
         [HttpGet]
         public IActionResult Insert()
         {
@@ -52,50 +51,52 @@ namespace FPTBook.Controllers
           return View(viewModel);
         }
 
-    [HttpPost]
-    public async Task<IActionResult> Insert(BookGenreViewModel viewModel)
-    {
-      if (!ModelState.IsValid)
-      {
-        viewModel = new BookGenreViewModel
+        [HttpPost]
+        public async Task<IActionResult> Insert(BookGenreViewModel viewModel)
         {
-          Genres = _context.Genres.ToList()
-        };
-        return View(viewModel);
-      }
-      using (var memoryStream = new MemoryStream())
-      {
-        await viewModel.FormFile.CopyToAsync(memoryStream);
-        var newBook = new Book
+            if (!ModelState.IsValid)
+            {
+                viewModel = new BookGenreViewModel
+                {
+                    Genres = _context.Genres.ToList()
+                };
+                return View(viewModel);
+            }
+            using (var memoryStream = new MemoryStream())
+            {
+                await viewModel.FormFile.CopyToAsync(memoryStream);
+                var newBook = new Book
+                {
+                    Title = viewModel.Book.Title,
+                    Price = viewModel.Book.Price,
+                    Author = viewModel.Book.Author,
+                    GenreId = viewModel.Book.GenreId,
+                    BookStatus = Enums.BookStatus.inStock,
+                    ImageData = memoryStream.ToArray()
+                };
+                _context.Add(newBook);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Update(int id)
         {
-          Title = viewModel.Book.Title,
-          Price = viewModel.Book.Price,
-          Author = viewModel.Book.Author,
-          GenreId = viewModel.Book.GenreId,
-          ImageData = memoryStream.ToArray()
-        };
-        _context.Add(newBook);
-        await _context.SaveChangesAsync();
-      }
-      return RedirectToAction("Index");
-    }
+            var bookInDb = _context.Books.SingleOrDefault(t => t.BookId == id);
+            if (bookInDb is null)
+            {
+                return NotFound();
+            }
 
-    [HttpGet]
-    public IActionResult Update(int id)
-    {
-      var bookInDb = _context.Books.SingleOrDefault(t => t.BookId == id);
-      if (bookInDb is null)
-      {
-        return NotFound();
-      }
+            var viewModel = new BookGenreViewModel()
+            {
+                Book = bookInDb,
+                Genres = _context.Genres.ToList()
+            };
+            return View(viewModel);
+        }
 
-      var viewModel = new BookGenreViewModel()
-      {
-        Book = bookInDb,
-        Genres = _context.Genres.ToList()
-      };
-      return View(viewModel);
-    }
         [HttpPost]
         public async Task<IActionResult> Update(BookGenreViewModel viewModel)
         {
@@ -122,13 +123,13 @@ namespace FPTBook.Controllers
                 bookInDb.Price = viewModel.Book.Price;
                 bookInDb.GenreId = viewModel.Book.GenreId;
                 bookInDb.ImageData = memoryStream.ToArray();
-
+                
             }
 
             _context.SaveChanges();
             return RedirectToAction("Index");
         }
-            [HttpGet]
+        [HttpGet]
         public IActionResult Details(int id)
         {
             var bookInDb = _context.Books
@@ -163,10 +164,11 @@ namespace FPTBook.Controllers
         [HttpGet]
         public IActionResult GenreList()
         {
-            IEnumerable<Genre> genres = _context.Genres.ToList();
+            var genres = _context.Genres.Include(t => t.Books).ToList();
 
             return View(genres);
         }
+
         [HttpGet]
         public IActionResult GenreRequest()
         {
@@ -185,7 +187,14 @@ namespace FPTBook.Controllers
             _context.Add(genre);
 
             await _context.SaveChangesAsync();
-            return View("GenreList");
+            return RedirectToAction("GenreList");
+        }
+        public async Task<IActionResult> OrderList()
+        {
+            var orderList = _context.Orders.Include(t=> t.CartList).ToList();
+
+            await _context.SaveChangesAsync();
+            return View(orderList.AsEnumerable());
         }
     }
 }
